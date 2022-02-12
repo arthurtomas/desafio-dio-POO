0c97c35a5ea5cb7732eb64ccfc59cf526dbf44ee
arthurtomas <arthurtomas.ep@gmail.com>
2022-02-12 14:55:09 -0300

    Classe Main, Curso e Mentoria adicionados, junto com arquivos adicionais

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+package src;
+
+import src.br.com.dio.desafio.dominio.Curso;
+import src.br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de Javascript");
+        curso2.setDescricao("Descrição curso Javascript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
+    }
+}
